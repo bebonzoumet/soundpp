12444ad8bfc5c1e0ed4e30eea4d9e0bdf730f683
mathjannotti <118695505+mathjannotti@users.noreply.github.com>
2022-11-21 19:54:22 -0300

    Add files via upload

---
+++ InsercaoOuvinte.java
@@ -0,0 +1,48 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class InsercaoOuvinte {
+    public static void main (String[] args) throws SQLException{
+        ListagemPlano lp = new ListagemPlano();
+        Scanner scNome = new Scanner(System.in);
+        System.out.println("Digite o nome do ouvinte: ");
+        String nome = scNome.nextLine();
+
+        while(nome.contains(";")){
+            System.out.println("Nome Invalido!");
+            System.out.println("Digite o nome do ouvinte: ");
+            nome = scNome.nextLine();
+        }
+
+        Scanner scCPF = new Scanner(System.in);
+        System.out.println("Digite o CPF do ouvinte: ");
+        String cpf = scCPF.nextLine();
+
+        while(cpf.contains(";")){
+            System.out.println("CPF Invalido!");
+            System.out.println("Digite o CPF do administrador: ");
+            cpf = scCPF.nextLine();
+        }
+
+        lp.ListagemPlano();
+        Scanner plano_id = new Scanner(System.in);
+		System.out.println("Digite o id do plano: ");
+		int plano = plano_id.nextInt();
+
+        CriaConexao criaConexao = new CriaConexao();
+        Connection connection = criaConexao.recuperarConexao();
+        Statement stm = connection.createStatement(); 
+        stm.execute(("insert into Ouvinte (fk_plano_assinatura, nome, cpf) values (" + "'" + plano + "'" +  "," + "'" + nome + "'" +  "," + "'" + cpf + "'" + ");") , Statement.RETURN_GENERATED_KEYS);
+        ResultSet rst = stm.getGeneratedKeys();
+        while (rst.next()){
+            Integer id = rst.getInt(1); 
+            System.out.println(id);
+        }
+        connection.close();
+        scNome.close();
+        scCPF.close();
+    }
+}
